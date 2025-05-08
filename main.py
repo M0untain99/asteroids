b338a307a6f2eb99c1da1afdb04753f7d852faa3
M0untain99 <dmountain52@gmail.com>
2025-05-08 15:32:23 +0100

    Set the FPS to 60

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
 
     pygame.init()  # Initialise PyGame
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Define the screen
+    clock = pygame.time.Clock  # Game Clock for controlling fps
+    dt = 0  # Delta Time
     
     while True:  # Initialise Game Loop
         for event in pygame.event.get():
@@ -16,6 +18,9 @@ def main():
 
         screen.fill('black')  # Make the screen black
         pygame.display.flip()  # Display the screen
+
+        time_passed = clock.tick(60)  # Set FPS to 60
+        dt = time_passed/1000
 
 
 if __name__ == "__main__":
