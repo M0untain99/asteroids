04d03761f1f3e74c889a1a9268d090d207e10896
M0untain99 <dmountain52@gmail.com>
2025-05-08 15:46:14 +0100

    Added player sprite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print('Starting Asteroids!')
@@ -8,8 +9,9 @@ def main():
 
     pygame.init()  # Initialise PyGame
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Define the screen
-    clock = pygame.time.Clock  # Game Clock for controlling fps
+    clock = pygame.time.Clock()  # Game Clock for controlling fps
     dt = 0  # Delta Time
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     
     while True:  # Initialise Game Loop
         for event in pygame.event.get():
@@ -17,8 +19,9 @@ def main():
                 return  # Kill the game
 
         screen.fill('black')  # Make the screen black
+        player.draw(screen)  # Draw Player
         pygame.display.flip()  # Display the screen
-
+        
         time_passed = clock.tick(60)  # Set FPS to 60
         dt = time_passed/1000
 
